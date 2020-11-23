08b0bd5865e93c421ca1c92e5cfc61b53f2ed261
chetan <zambare.c123@gmail.com>
2020-11-23 07:49:32 +0530

    Welcome meaasge

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
 
 public class UserRegistration {
-
+	public static void main(String[] args) {
+		 Scanner sc=new Scanner(System.in);
+		System.out.println("Welcome to user registartion ");
+	}
 }
