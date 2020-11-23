b36c2e3b942d7f84fdb973e32abc8d2ab58d8f78
chetan <zambare.c123@gmail.com>
2020-11-23 08:01:36 +0530

    User Last name check

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -6,18 +6,28 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		 Scanner sc=new Scanner(System.in);
 			System.out.println("Welcome to user registartion ");
-			String InputPattern = "^(?=.*[A-Z]{1}[a-z A-Z]){3,}$";
-			Pattern pattern = Pattern.compile(InputPattern); 
-			System.out.println("Enter text:");
-			Matcher matcher = pattern.matcher(sc.nextLine());    
-			boolean found = false;    
-	        while (matcher.find()) {    
-	            System.out.println("I found the text "+matcher.group()+" starting at index "+    
-	             matcher.start()+" and ending at index "+matcher.end());    
-	            found = true;    
-	        }    
-	        if(!found){    
-	            System.out.println("No match found.");    
-	        } 
+			String InputPattern = "^(?=.*[A-Z]{1}[a-z A-Z]){3,}$";  // fisrt name pattern
+			System.out.println("Enter first name :");
+			String InputFirstName = sc.next();
+			check(InputPattern,InputFirstName);
+			String InputPattern2 = "^(?=.*[A-Z]{1}[a-z A-Z]){3,}$";  // fisrt name pattern
+			System.out.println("Enter Last name :");
+			String InputLastName = sc.next();
+			check(InputPattern2,InputLastName);
+			
+			
+	}
+	public static void check(String InputPattern , String InputName) { 
+		Pattern pattern = Pattern.compile(InputPattern); 
+		Matcher matcher = pattern.matcher(InputName);    
+		boolean found = false;    
+        while (matcher.find()) {    
+            System.out.println("I found the text "+matcher.group()+" starting at index "+    
+             matcher.start()+" and ending at index "+matcher.end());    
+            found = true; 
+        }    
+        if(!found){    
+            System.out.println("No match found.");  
+        } 
 	}
 }
