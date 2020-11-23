8dc0ecbc74cb8055f5e7b6c91dec4c3c9c877133
chetan <zambare.c123@gmail.com>
2020-11-23 09:30:41 +0530

    [Chetan Added]: First name Check

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -5,25 +5,14 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 	public static void main(String[] args) {
 		 Scanner sc=new Scanner(System.in);
-			System.out.println("Welcome to user registartion ");
-			String InputPattern = "^(?=.*[A-Z]{1}[a-z A-Z]){3,}$";  // fisrt name pattern
-			System.out.println("Enter first name :");
-			String InputFirstName = sc.next();
-			check(InputPattern,InputFirstName);
-			
-			
-	}
-	public static void check(String InputPattern , String InputName) { 
-		Pattern pattern = Pattern.compile(InputPattern); 
-		Matcher matcher = pattern.matcher(InputName);    
-		boolean found = false;    
-        while (matcher.find()) {    
-            System.out.println("I found the text "+matcher.group()+" starting at index "+    
-             matcher.start()+" and ending at index "+matcher.end());    
-            found = true; 
-        }    
-        if(!found){    
-            System.out.println("No match found.");  
-        } 
+			System.out.println("Enter your first name: ");
+		      String name = sc.next();
+		      String regex = "[A-Z]{1}[a-zA-Z]{2,}";
+		      boolean result = name.matches(regex);
+		      if(result) {
+		         System.out.println("Given name is valid.");
+		      } else {
+		         System.out.println("Given name is not valid.");
+		      }	
 	}
 }
